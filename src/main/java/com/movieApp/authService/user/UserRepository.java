23482bd8160e433ff6255c594af811000a6f186c
Miro29147 <miro29147@gmail.com>
2024-01-29 12:38:59 +0200

    Initial commit

---
+++ src/main/java/com/movieApp/authService/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.movieApp.authService.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+}
---
+++ src/test/java/com/movieApp/authService/MovieAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieApp.authService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
